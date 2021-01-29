f69efa34000eb9ae0be50c3403974ff448c0eb78
Lopez-John <johncarlolopez.88@gmail.com>
2021-01-28 20:07:14 -0500

    updated everything

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,27 +1,72 @@
 """Main app/routing file for Twitoff."""
 
-from flask import Flask, render_template
-from .models import DB, User, insert_example_users
+from os import getenv
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import update_all_users, add_or_update_user
+
+from .predict import predict_user
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE-URI'] = 'sqlite:///db.sqlite3'
-    app.congfig['SQLALCHEMY_TRACKING_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     DB.init_app(app)
 
-
     @app.route('/')
-    def root:
-        DB.drop_all()
-        DB.create_all()
-        insert_example_users()
+    def root():
+        # DB.drop_all()
+        # DB.create_all()
         return render_template('base.html', title='Home',
                                 users=User.query.all())
 
-    @app.route('/goodbye')
-    def goodby():
-        return 'Goodbye Twitoff!'
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=["Get"])
+    def user(name=None, message=""):
+        name = name or request.values['user_name']
+        try: 
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} was successfully added!'.format(name)
+            
+            tweets = User.query.filter(User.name == name).one().tweets
+        
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
 
+        return render_template(
+            'user.html',title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user0, user1 = sorted(
+            [request.values['user1'], request.values['user2']])
+
+        if user0 == user1:
+            message = 'cannot compare users to themselves'
+        
+        else:
+            #predicts user
+            prediction = predict_user(user0, user1, request.values['tweet_text'])
+            message = '{} is the more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], 
+                user1 if prediction else user0,
+                user0 if prediction else user1
+                )
+        return render_template('prediction.html', title='Prediction', message=message)
+
+    @app.route('/update')
+    def update():
+        update_all_users()
+        return render_template('base.html', title='Home',
+                                users=User.query.all())
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Home')
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,24 +1,29 @@
+"""SQLAlchemy models and utility functions for Twitoff"""
+
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
 
-class User(DB.Model): # User Table
-    '''Twitter Users corresponding to Tweets'''
+class User(DB.Model):  # User Table
+
+    """Twitter Users corresponding to Tweets"""
     id = DB.Column(DB.BigInteger, primary_key=True)  # id column
-    name = DB.Column(DB.STring, nullable=False)  # name column
+    name = DB.Column(DB.String, nullable=False)  # name column
+    newest_tweet_id = DB.Column(DB.BigInteger) # keeps track of newest tweet of user
+
 
     def __repr__(self):
-        return'User: {}>'.format(self.name)
+        return "<User: {}>".format(self.name)
 
 
 class Tweet(DB.Model):
-    '''Tweets corresponding to Users'''
-    id = DB.Column(DB.BigInteger, primary_key=True) # id column
-    test = DB.Column(DB.Unicode(300))  # tweet text column - allows for emojis
-    user_id = DB.Column(DB.BigInteger, DB.ForiegnKey(
-        'user.id'), nullable=False)
-    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    """Tweets corresponding to Users"""
+    id = DB.Column(DB.BigInteger, primary_key=True)  # id column
+    text = DB.Column(DB.Unicode(300))  # tweet text column - allows for emojis
+    vect = DB.Column(DB.PickleType, nullable=False) # vectorize twets
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey("user.id"), nullable=False)  # user_id column (corresponding user)
+    user = DB.relationship("User", backref=DB.backref("tweets", lazy=True))  # creates user link between tweets
 
     def __repr__(self):
-        return '<Tweet: {}>.format(self.text)
+        return "<Tweet: {}>".format(self.text)
